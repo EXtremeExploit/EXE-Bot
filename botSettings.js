cd74bf4881e6f707463444b7eb701ce25200c81f
EXtremeExploit <elalfagamermc@gmail.com>
2017-10-27 23:26:59 -0300

    Add files via upload

---
+++ botSettings.js
@@ -0,0 +1,21 @@
+const json = require("./settings.json");
+class settings {
+    token(){
+        return json.token;
+    }
+    prefix(){
+        return json.prefix;
+    }
+    osuApiKey(){
+        return json.osuApiKey;
+    }
+    ownerID(){
+        return json.ownerID;
+    }
+    allEvents(){
+        return json.allEvents;
+    }
+    debug(){
+        return json.debug;
+    }
+}module.exports = settings;

89ca30f7fe633f2071e2e298594a5e5bb9b5de5f
EXtremeExploit <elalfagamermc@gmail.com>
2017-11-07 19:01:58 -0300

    reverse command

--- index.js
+++ index.js
@@ -66,7 +66,10 @@ new events(client,debug,allEvents,prefix);
 
 function clean(text) {
     if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+      return text.replace(/`/g, String.fromCharCode(8203))
+      .replace(/@/g, String.fromCharCode(8203))
+      .replace(token, "*TOKEN*")
+      .replace(osuApiKey, "*OSUAPIKEY*");
     else
         return text;
 }
@@ -117,7 +120,7 @@ client.on("message", (msg) => {
         .addField("Info","**server:** Info about the server \n**role:** Info about a role \n**channel:** Info about a channel\n**user:** Info about you \n**avatar:** Gets your AvatarURL",true)
         .addField("Random","**roll:** Rolls a dice\n**rate:** Rates something \n**8ball:**  Asks the 8ball a question \n**cat:** Gets a random cat image",true)
         .addField("Moderation", "**kick:** Kicks someone \n**ban:** Bans someone \n**purge:** Deletes a count of messages in a channel")
-        .addField("Fun","**say:** Says whatever you want \n**lenny:** Displays the lenny face\n**cookie**: Gives a cookie to someone",true)
+        .addField("Fun","**say:** Says whatever you want \n**lenny:** Displays the lenny face\n**cookie**: Gives a cookie to someone\n**reverse:** Returns the given text but reversed",true)
         .addField("Osu", "**osuStdUser**: Gets info about an user in the Standard mode \n**osuTaikoUser**: Gets info about an user in the Taiko mode \n**osuCtbUser**: Gets info about an user in the CatchTheBeat mode \n**osuManiaUser**: Gets info about an user in the Mania mode \n**osuStdBest:** Gets the best play of an user in the Standard mode \n**osuTaikoBest:** Gets the best play of an user in the Taiko mode \n**osuCtbBest:** Gets the best play of an user in the CatchTheBeat mode \n**osuManiaBest:** Gets the best play of an user in the mania mode \n**osuBeatmap**: Gets info about an osu!beatmap", true)
         .addField("Misc","**ping:** Pings the bot and the discord API",true)
         .addField("Wiki","[Wiki](https://github.com/EXtremeExploit/EXE-Bot/wiki/)\n[Wiki: Commands](https://github.com/EXtremeExploit/EXE-Bot/wiki/Commands)");

a22ccdc08efd2dd4b828a1edf86dc33c949f0dfa
EXtremeExploit <gdextremeexploit@gmail.com>
2017-12-10 16:18:17 -0300

    trying to fix skip

--- scripts/voiceCommands.js
+++ scripts/voiceCommands.js
@@ -63,12 +63,10 @@ class voiceCommands {
                 if(server.dispatcher) {
                     if(server.queue.length == 0){
                         msg.guild.voiceConnection.disconnect();
-                        server.dispatcher.end();
                         msg.channel.send(new discord.RichEmbed()
                         .setColor([255,0,0])
                         .setDescription('Skipped, and queue is empty, so i left the voice channel'));
                     }
-                    server.dispatcher.end()
                     msg.channel.send(new discord.RichEmbed()
                     .setColor([255,0,0])
                     .setDescription('Skipped!'));

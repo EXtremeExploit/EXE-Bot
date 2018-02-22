4f2d7d856b31bc160c3a85afc74bef398cad5c53
EXtremeExploit <gdextremeexploit@gmail.com>
2018-02-21 23:10:50 -0300

    Aded Pages, Hardware usages and Counts

--- commands/Support/info.js
+++ commands/Support/info.js
@@ -33,10 +33,21 @@ class info {
             .setAuthor(client.user.username, client.user.avatarURL)
             .setColor([255, 0, 0])
             .setThumbnail(client.user.avatarURL)
+            .setImage('https://discordbots.org/api/widget/353661793199194112.png?topcolor=c90000&middlecolor=b70000&usernamecolor=ffffff&datacolor=FFFFFF&labelcolor=870000&highlightcolor=c90000&certifiedcolor=fffffff')
+            .addField('Links', '[**Discord Bots**](https://discordbots.org/bot/353661793199194112)\n' +
+                '[**Page**](https://extremeexploit.github.io/EXE_Bot)\n' +
+                '[**Discord Server**](https://discord.gg/sJPmDDn)\n' +
+                '[**Github Repository**](https://github.com/EXtremeExploit/EXE-Bot)', true)
+            .addField('Usages', '**RAM:** ' + (process.memoryUsage().heapUsed / 1014 / 1024).toFixed(3) + 'MB\n' +
+                '**CPU-System:** ' + process.cpuUsage().system + '\n' +
+                '**CPU-User:** ' + process.cpuUsage().user, true)
+            .addField('Counts', '**Servers:** ' + client.guilds.size + '\n' +
+                '**Users:** ' + client.users.size + '\n' +
+                '**Channels:** ' + client.channels.size, true)
             .addField('Wikies', '[**Home**](' + wikis.home + ')\n' +
-            '[**Commands**](' + wikis.commands + ')\n' +
-            '[**Replies**](' + wikis.replies + ')\n' +
-            '[**FAQ**](' + wikis.faq + ')'));
+                '[**Commands**](' + wikis.commands + ')\n' +
+                '[**Replies**](' + wikis.replies + ')\n' +
+                '[**FAQ**](' + wikis.faq + ')', true)).catch((e) => console.log(e));
     }
 }
 module.exports = info;

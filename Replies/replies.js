582d7cbffd81f54ba29d45e00da620b5940864ef
EXtremeExploit <elalfagamermc@gmail.com>
2018-06-16 03:19:59 -0300

    Add files via upload

---
+++ Replies/replies.js
@@ -0,0 +1,20 @@
+const main = new (require('../scripts/scripts')).Main();
+const data = main.getData();
+const { Client } = require('discord.js');
+class Replies {
+    /**
+     * 
+     * @param {Client} client 
+     */
+    constructor(client) {
+        this.standard = require('./standard');
+        this.osu = require('./osu');
+        if (data.replies().standard == true || data.replies().standard == 'true') {
+            new this.standard(client);
+            if (data.replies().osu == true || data.replies().osu == 'true') {
+                new this.osu(client);
+            }
+        }
+    }
+}
+module.exports = Replies;

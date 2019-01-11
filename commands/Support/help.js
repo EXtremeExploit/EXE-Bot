1d1a389a3c2432839cdda535b228c2ccda0515f1
EXtremeExploit <gdextremeexploit@gmail.com>
2019-01-11 20:01:40 -0300

    Passed commands to webpage so it doesn't take 99% of the chat

--- commands/Support/help.js
+++ commands/Support/help.js
@@ -29,52 +29,6 @@ class help {
 					.setTitle(`${client.user.username} Commands`);
 				//#endregion
 
-				//#region Support
-				if (data.commands().categories.Support == true || data.commands().categories.Support == 'true') {
-					embed.addField('Support', commands.support, true);
-				}
-				//#endregion
-				//#region Fun
-				if (data.commands().categories.Fun == true || data.commands().categories.Fun == 'true') {
-					embed.addField('Fun', commands.fun, true);
-				}
-				//#endregion
-				//#region Games
-				if (data.commands().categories.Games == true || data.commands().categories.Games == 'true') {
-					embed.addField('Games', commands.games, true);
-				}
-				//#endregion
-				//#region Info
-				if (data.commands().categories.Info == true || data.commands().categories.Info == 'true') {
-					embed.addField('Info', commands.info, true);
-				}
-				//#endregion
-				//#region Misc
-				if (data.commands().categories.Misc == true || data.commands().categories.Misc == 'true') {
-					embed.addField('Misc', commands.misc, true);
-				}
-				//#endregion
-				//#region Moderation
-				if (data.commands().categories.Moderation == true || data.commands().categories.Moderation == 'true') {
-					embed.addField('Moderation', commands.moderation, true);
-				}
-				//#endregion
-				//#region NSFW
-				if (data.commands().categories.NSFW == true || data.commands().categories.NSFW == 'true') {
-					embed.addField('NSFW', commands.nsfw, true);
-				}
-				//#endregion
-				//#region Random
-				if (data.commands().categories.Random == true || data.commands().categories.Random == 'true') {
-					embed.addField('Random', commands.random, true);
-				}
-				//#endregion
-				//#region Utility
-				if (data.commands().categories.Utility == true || data.commands().categories.Utility == 'true') {
-					embed.addField('Utility', commands.utility, true);
-				}
-				//#endregion
-
 				//#region Wiki
 				if (data.commands().categories.Wiki == true || data.commands().categories.Wiki == 'true') {
 					embed.addField('Wiki', commands.wiki, true);
@@ -82,16 +36,7 @@ class help {
 				//#endregion
 
 				//#region No Commands
-				if (!data.commands().categories.Fun == true &&
-					!data.commands().categories.Info == true &&
-					!data.commands().categories.Misc == true &&
-					!data.commands().categories.Moderation == true &&
-					!data.commands().categories.NSFW == true &&
-					!data.commands().categories.Osu == true &&
-					!data.commands().categories.Random == true &&
-					!data.commands().categories.Support == true &&
-					!data.commands().categories.Wiki == true &&
-					!data.commands().categories.Utility == true) {
+				if (!data.commands().categories.Wiki == true) {
 					embed.setDescription('I don\'t have any commands...')
 						.setFooter('Commands? what is that?');
 				}

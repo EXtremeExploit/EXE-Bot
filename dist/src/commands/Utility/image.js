244bd8aec7366618b999afdb2dfe53c3840ea7ee
EXtremeExploit <gdextremeexploit@gmail.com>
2020-09-10 10:14:42 -0300

    Optimize image command

--- dist/src/commands/Utility/image.js
+++ dist/src/commands/Utility/image.js
@@ -5,11 +5,6 @@ let wikis = new config().GetWikis();
 let googleKeys = new config().GetGoogle();
 import _gimages from 'google-images';
 const gimages = new _gimages(googleKeys.cseID, googleKeys.appApiKey);
-const firstemoji = '⏮';
-const backemoji = '◀';
-const nextemoji = '▶';
-const lastemoji = '⏭';
-const stopemoji = '⏹';
 export default class {
     constructor(client, msg) {
         let messageArray = msg.content.split(' ');
@@ -30,88 +25,63 @@ export default class {
                         .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
                         .setColor([255, 0, 0])
                         .setTitle('Error')
-                        .setDescription('• Could not find any image on your search'));
+                        .setDescription('Could not find any image on your search'));
                     return;
                 }
+                //Arguments are given and an image was found
                 let index = 0;
                 let embed = new discord.MessageEmbed()
                     .setColor([255, 0, 0])
                     .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-                    .setDescription('Displaying ' + index + 1 + '/' + images.length + ' Images')
+                    .setDescription(`Displaying ${index + 1}/${images.length} Images`)
                     .setURL(images[index].url)
                     .setTitle('Google Image Search')
                     .setImage(images[index].url)
                     .setFooter('Getting reactions');
+                //Send the embed which contains the first image
                 msg.channel.send(embed).then(async (message) => {
-                    await this.reactButtons(message);
-                    const collector = message.createReactionCollector((reaction, user) => user.id != client.user.id, {
+                    //Create reaction buttons, sleep is to avoid rate limit
+                    await message.react('⏮');
+                    await sleep(750);
+                    await message.react('◀');
+                    await sleep(750);
+                    await message.react('▶');
+                    await sleep(750);
+                    await message.react('⏭');
+                    await sleep(750);
+                    await message.react('⏹');
+                    await sleep(750);
+                    const collector = message.createReactionCollector((reaction, user) => user.id == msg.author.id, {
                         time: 60000
                     });
                     collector.on('collect', async (reaction, reactions) => {
                         if (reaction.count >= 2) {
-                            if (reaction.emoji.name == firstemoji) {
+                            switch (reaction.emoji.name) {
+                                case '⏮':
+                                    index = 0;
+                                    break;
+                                case '⏭':
+                                    index = (images.length - 1);
+                                    break;
+                                case '⏹':
+                                    collector.stop('User requested');
+                                    index = index;
+                                    return;
+                                case '◀':
+                                    index -= 1;
+                                    break;
+                                case '▶': index += 1;
+                            }
+                            if (index < 0)
                                 index = 0;
-                                embed = new discord.MessageEmbed()
-                                    .setColor([255, 0, 0])
-                                    .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-                                    .setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-                                    .setURL(images[index].url)
-                                    .setTitle('Google Image Search')
-                                    .setImage(images[index].url)
-                                    .setFooter('Getting reactions');
-                                await message.edit(embed);
-                                await message.reactions.removeAll();
-                                await this.reactButtons(message);
-                            }
-                            if (reaction.emoji.name == lastemoji) {
+                            if (index > (images.length - 1))
                                 index = (images.length - 1);
-                                embed = new discord.MessageEmbed()
-                                    .setColor([255, 0, 0])
-                                    .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-                                    .setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-                                    .setURL(images[index].url)
-                                    .setTitle('Google Image Search')
-                                    .setImage(images[index].url)
-                                    .setFooter('Getting reactions');
-                                await message.edit(embed);
-                                await message.reactions.removeAll();
-                                await this.reactButtons(message);
-                            }
-                            if (reaction.emoji.name == stopemoji) {
-                                collector.stop('User requested');
-                            }
-                            if (reaction.emoji.name == backemoji) {
-                                if (index == 0)
-                                    index = index + 1;
-                                index = (index - 1);
-                                embed = new discord.MessageEmbed()
-                                    .setColor([255, 0, 0])
-                                    .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-                                    .setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-                                    .setURL(images[index].url)
-                                    .setTitle('Google Image Search')
-                                    .setImage(images[index].url)
-                                    .setFooter('Getting reactions');
-                                await message.edit(embed);
-                                await message.reactions.removeAll();
-                                await this.reactButtons(message);
-                            }
-                            if (reaction.emoji.name == nextemoji) {
-                                if (index == 9)
-                                    index = index - 1;
-                                index = (index + 1);
-                                embed = new discord.MessageEmbed()
-                                    .setColor([255, 0, 0])
-                                    .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-                                    .setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-                                    .setURL(images[index].url)
-                                    .setTitle('Google Image Search')
-                                    .setImage(images[index].url)
-                                    .setFooter('Getting reactions');
-                                await message.edit(embed);
-                                await message.reactions.removeAll();
-                                await this.reactButtons(message);
-                            }
+                            embed.setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
+                                .setURL(images[index].url)
+                                .setImage(images[index].url);
+                            await message.edit(embed);
+                            reaction.users.remove(msg.author.id);
+                            await message.react(reaction.emoji.name);
                         }
                     });
                     collector.on('end', (coll, reason) => {
@@ -122,17 +92,5 @@ export default class {
                 });
             });
         }
-    }
-    async reactButtons(message) {
-        await message.react(firstemoji);
-        await sleep(750);
-        await message.react(backemoji);
-        await sleep(750);
-        await message.react(nextemoji);
-        await sleep(750);
-        await message.react(lastemoji);
-        await sleep(750);
-        await message.react(stopemoji);
-        await sleep(750);
     }
 }
--- src/commands/Utility/image.ts
+++ src/commands/Utility/image.ts
@@ -6,13 +6,6 @@ let googleKeys = new config().GetGoogle();
 import _gimages from 'google-images';
 const gimages = new _gimages(googleKeys.cseID, googleKeys.appApiKey);
 
-const firstemoji = '⏮';
-const backemoji = '◀';
-const nextemoji = '▶';
-const lastemoji = '⏭';
-const stopemoji = '⏹';
-
-
 export default class {
 	constructor(client: discord.Client, msg: discord.Message) {
 		let messageArray = msg.content.split(' ');
@@ -33,94 +26,65 @@ export default class {
 						.setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
 						.setColor([255, 0, 0])
 						.setTitle('Error')
-						.setDescription('• Could not find any image on your search'));
+						.setDescription('Could not find any image on your search'));
 					return;
 				}
+
+				//Arguments are given and an image was found
 				let index = 0;
 				let embed = new discord.MessageEmbed()
 					.setColor([255, 0, 0])
 					.setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-					.setDescription('Displaying ' + index + 1 + '/' + images.length + ' Images')
+					.setDescription(`Displaying ${index + 1}/${images.length} Images`)
 					.setURL(images[index].url)
 					.setTitle('Google Image Search')
 					.setImage(images[index].url)
 					.setFooter('Getting reactions');
 
+				//Send the embed which contains the first image
 				msg.channel.send(embed).then(async (message) => {
-					await this.reactButtons(message);
+					//Create reaction buttons, sleep is to avoid rate limit
+					await message.react('⏮');
+					await sleep(750);
+					await message.react('◀');
+					await sleep(750);
+					await message.react('▶');
+					await sleep(750);
+					await message.react('⏭');
+					await sleep(750);
+					await message.react('⏹');
+					await sleep(750);
 
-					const collector = message.createReactionCollector((reaction, user) => user.id != client.user.id,
-						{
-							time: 60000
-						});
+					const collector = message.createReactionCollector((reaction, user) => user.id == msg.author.id, {
+						time: 60000
+					});
 
 					collector.on('collect', async (reaction, reactions) => {
 						if (reaction.count >= 2) {
-							if (reaction.emoji.name == firstemoji) {
-								index = 0;
-								embed = new discord.MessageEmbed()
-									.setColor([255, 0, 0])
-									.setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-									.setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-									.setURL(images[index].url)
-									.setTitle('Google Image Search')
-									.setImage(images[index].url)
-									.setFooter('Getting reactions');
-								await message.edit(embed);
-								await message.reactions.removeAll();
+							switch (reaction.emoji.name) {
+								case '⏮': index = 0;
+									break;
+								case '⏭': index = (images.length - 1);
+									break;
+								case '⏹':
+									collector.stop('User requested');
+									index = index;
+									return;
+								case '◀': index -= 1;
+									break;
+								case '▶': index += 1;
+							}
 
-								await this.reactButtons(message);
-							}
-							if (reaction.emoji.name == lastemoji) {
-								index = (images.length - 1);
-								embed = new discord.MessageEmbed()
-									.setColor([255, 0, 0])
-									.setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-									.setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-									.setURL(images[index].url)
-									.setTitle('Google Image Search')
-									.setImage(images[index].url)
-									.setFooter('Getting reactions');
-								await message.edit(embed);
-								await message.reactions.removeAll();
+							if (index < 0) index = 0;
+							if (index > (images.length - 1)) index = (images.length - 1);
 
-								await this.reactButtons(message);
-							}
-							if (reaction.emoji.name == stopemoji) {
-								collector.stop('User requested');
-							}
-							if (reaction.emoji.name == backemoji) {
-								if (index == 0) index = index + 1;
-								index = (index - 1);
-								embed = new discord.MessageEmbed()
-									.setColor([255, 0, 0])
-									.setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-									.setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-									.setURL(images[index].url)
-									.setTitle('Google Image Search')
-									.setImage(images[index].url)
-									.setFooter('Getting reactions');
-								await message.edit(embed);
-								await message.reactions.removeAll();
+							embed.setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
+								.setURL(images[index].url)
+								.setImage(images[index].url)
+							await message.edit(embed);
 
-								await this.reactButtons(message);
-							}
-							if (reaction.emoji.name == nextemoji) {
-								if (index == 9) index = index - 1;
-								index = (index + 1);
-								embed = new discord.MessageEmbed()
-									.setColor([255, 0, 0])
-									.setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true, size: 1024, format: `png` }))
-									.setDescription('Displaying ' + (index + 1) + '/' + images.length + ' Images')
-									.setURL(images[index].url)
-									.setTitle('Google Image Search')
-									.setImage(images[index].url)
-									.setFooter('Getting reactions');
-								await message.edit(embed);
-								await message.reactions.removeAll();
-
-								await this.reactButtons(message);
-							}
+							reaction.users.remove(msg.author.id);
+							await message.react(reaction.emoji.name);
 						}
 					});
 					collector.on('end', (coll, reason) => {
@@ -131,17 +95,5 @@ export default class {
 				});
 			});
 		}
-	}
-	async reactButtons(message) {
-		await message.react(firstemoji);
-		await sleep(750);
-		await message.react(backemoji);
-		await sleep(750);
-		await message.react(nextemoji);
-		await sleep(750);
-		await message.react(lastemoji);
-		await sleep(750);
-		await message.react(stopemoji);
-		await sleep(750);
 	}
 }
